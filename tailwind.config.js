82b037292ec3830e14ddbfbe3f3bc7efe67daa83
= <=>
2023-08-25 14:08:11 +0300

    draft build

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    colors: {
+      purple: '#565BEF',
+      'light-purple': '#A722F4',
+      blue: '#2e10b6',
+      white: colors.white,
+      black: colors.black,
+      gray: colors.gray,
+      transparent: colors.transparent,
+      body: '#6f7c8a',
+    },
+    fontFamily: {
+      rubik: "'Rubik', sans-serif",
+      mulish: "'Mulish', sans-serif",
+    },
+    extend: {
+      fontSize: {
+        '4.5xl': '2.5rem',
+      },
+    },
   },
   plugins: [],
-}
-
+};
